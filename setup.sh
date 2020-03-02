ab35706d9ea495c630a7ee82e578c735c8adb249
Marantral <anthony.ralston27@gmail.com>
2020-03-02 16:21:19 -0500

    added setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 dpkg --add-architecture i386
 apt-get  update -y
-apt-get -y  install mono-complete mono-mcs unzip wget git ruby p7zip wine wine32 wine64 winbind
+apt-get -y  install mono-complete mono-mcs wine wine32 wine64 winbind
 
